e482afbe11c8a841f0421dfacb3ceeac24e21a18
DrSnowbird <DrSnowbird@openkbs.org>
2016-10-02 04:43:54 +0000

    update neo4j-docker using Centos7

--- docker/neo4j-docker/run-c7-neo4j-hdpclient.sh
+++ docker/neo4j-docker/run-c7-neo4j-hdpclient.sh
@@ -4,10 +4,11 @@
 #sudo docker run -d \
 DNS_SVR=`cat /etc/resolv.conf|cut -d' ' -f2-`
 sudo docker run -it \
-	-v /usr/hdp/2.4.0.0-169:/usr/hdp/2.4.0.0-169:rw \
-	-v /usr/hdp/current:/usr/hdp/current:rw \
-	-v /etc/hadoop:/etc/hadoop:rw \
-        --dns="${DNS_SVR}"
-        --network="bridge" \
-	-P local/c7-neo4j-hdpclient:latest
+        -v /usr/hdp/2.4.0.0-169:/usr/hdp/2.4.0.0-169:ro \
+        -v /usr/hdp/current:/usr/hdp/current:ro \
+        -v /etc/hadoop:/etc/hadoop:ro \
+        -v /usr/bin:/usr/hdp/bin/:ro \
+        --dns="${DNS_SVR}" \
+        --network="host" \
+	-P local/c7-neo4j-hdpclient
 
