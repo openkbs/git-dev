1c2320411f994ece6256839da749c26d242bff4b
openkbs <openkbs@mail.com>
2016-12-11 11:17:24 -0500

    added portainer.io startup scripts Sun Dec 11 11:17:23 EST 2016

--- bin/run-portainer-docker-server.sh
+++ bin/run-portainer-docker-server.sh
@@ -16,17 +16,51 @@ fi
 remote_host=${1}
 remote_windows=${2}
 
+#######################################################
+#### ---- Change this base port number as you like ----
+#######################################################
+port_base=19100
+
+
+#######################################################
+#######################################################
+#### ---- Mostly, you don't want to change below:  ----
+#######################################################
+#######################################################
+
+#### ---- clean up old/dead Portainer containers ----
+cleanupOld() {
+    ports_to_clean="`docker ps|grep portainer|grep "Exit"|awk '{print $1}'`"
+    if [ "$ports_to_clean" != "" ]; then
+        docker rm -f "$ports_to_clean"
+    fi
+}
+#cleanupOld
+
+#### ---- Find available Portainer containers ----
+port_use=19100
+findNextPort() {
+    ports_occupied=`docker ps|grep portainer|grep -v "Exit"|cut -d':' -f2|cut -d'-' -f1 | sort -u`
+    echo "... Min. base port: $port_base"
+    max=$port_base
+    for v in $ports_occupied; do
+        if (( $v > $max )); then max=$v; fi; 
+    done
+    port_use=$((max++))
+    echo "port_use=$port_use"
+}
+findNextPort
+
 if [ "$remote_host" == "" ]; then
     # Manage local Linux host
-    docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
+    docker run -d -p ${port_use}:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
 else
     if [ "$remote_windows" == "" ]; then
         # Manage remote Linux host
-        docker run -d -p 9000:9000 portainer/portainer -H tcp://mydockerhost.mydomain:2375
+        docker run -d -p ${port_use}:9000 portainer/portainer -H tcp://mydockerhost.mydomain:2375
     else
         # Manage remote Window host
-        docker run -d -p 9000:9000 portainer/portainer:windows -H tcp://mydockerhost.mydomain:2375
+        docker run -d -p ${port_use}:9000 portainer/portainer:windows -H tcp://mydockerhost.mydomain:2375
     fi
 fi
-
 
